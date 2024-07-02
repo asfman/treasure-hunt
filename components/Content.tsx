d0ef363639b1046ec5b8dcba9a4201d72f14f093
asfman <asfman@gmail.com>
2024-07-02 10:48:10 +0800

    card loading ui modified

--- components/Content.tsx
+++ components/Content.tsx
@@ -188,7 +188,7 @@ export function Game({ gameId }: {gameId: number}) {
 
   return (
     <div className="p-4 relative">
-      {!game && <div>loading</div>}
+      {!game && <Card className="bg-gray-200 flex items-center justify-center min-h-[520px] text-gray-400">loading...</Card>}
       {
         game &&
           <>
