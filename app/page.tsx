3513a9ebe39ed305e871d7e5de509d70e534e1ea
asfman <asfman@gmail.com>
2024-07-03 21:29:43 +0800

    add gmail in page's footer

--- app/page.tsx
+++ app/page.tsx
@@ -10,6 +10,7 @@ export default function Home() {
       <main>
         <Header />
         <Content />
+        <div className="flex mb-4 justify-center text-white">© Copyright 2024 • Treasure Hunt. any suggestions, please&nbsp;<a className="underline underline-offset-4 decoration-blue-600" href="mailto:asfman@gmail.com">send an email</a>.</div>
       </main>
   );
 }
